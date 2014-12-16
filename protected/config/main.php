5786112cf1bea941b1c5ed776c3873efa7894333
Andrey MAsyuk <andrey06945@gmail.com>
2014-12-16 22:51:09 +0300

    version 0.1

---
+++ protected/config/main.php
@@ -0,0 +1,137 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'KulibinTrade',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+                'application.modules.user.models.*',
+                //'application.modules.yiiadmin.models.*',
+                'application.modules.user.components.*',
+                'ext.yiiext.*',
+	),
+
+	'modules'=>array(
+                  // 'user',
+            'user'=>array(
+            # encrypting method (php hash function)
+            'hash' => 'md5',
+ 
+            # send activation email
+            'sendActivationMail' => true,
+ 
+            # allow access for non-activated users
+            'loginNotActiv' => false,
+ 
+            # activate user on registration (only sendActivationMail = false)
+            'activeAfterRegister' => false,
+ 
+            # automatically login from registration
+            'autoLogin' => true,
+ 
+            # registration path
+            'registrationUrl' => array('/user/registration'),
+ 
+            # recovery password path
+            'recoveryUrl' => array('/user/recovery'),
+ 
+            # login form path
+            'loginUrl' => array('/user/login'),
+ 
+            # page after login
+            'returnUrl' => array('/user/profile'),
+ 
+            # page after logout
+            'returnLogoutUrl' => array('/user/login'),
+        ),
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'123',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+            'yiiadmin'=>array(
+                'password'=>'123',
+                'registerModels'=>array(
+                    //'application.models.Contests',
+                    //'application.models.BlogPosts',
+                    'application.models.*',
+                ),
+                //'excludeModels'=>array(),
+            ),
+	),
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			//'class' => 'WebUser',
+			'allowAutoLogin'=>true,
+                        'loginUrl' => array('/user/login'),
+		),
+		// uncomment the following to enable URLs in path-format
+		
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		'shoppingCart' =>
+             array(
+          'class' => 'ext.yiiext.EShoppingCart',
+                   ),
+		/*'db'=>array(
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		),*/
+		// uncomment the following to use a MySQL database
+	
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=Kulibin',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => '123',
+			'charset' => 'utf8',
+                        'tablePrefix' => 'tbl_',
+		),
+		
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+	),
+);
